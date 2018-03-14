e2dc4f75e8364058770ef48bf8ca7fe0edddc4b7
Brendan Broderick <brendanbroderick01@gmail.com>
2018-03-14 17:37:09 -0500

    Get the initial csv file uploader working.

--- app/app.r
+++ app/app.r
@@ -3,6 +3,7 @@
 ###########################################
 library(shiny)
 library(shinythemes)
+library(tidyverse)
 library(flexsurv)
 library(quadprog)
 library(Hmisc)
@@ -14,18 +15,40 @@ source(here::here("pgm","utilsWts.r"))
 ###########################################
 ### User Interface
 ###########################################
+
 ui <- fluidPage(
   theme = shinytheme("united"),
   navbarPage(
-    title = "Milestone"
+    title = "Milestone",
+    tabPanel("Main",
+             sidebarPanel(
+               numericInput("nE", label = "Landmark Event Number", value = NA),
+               numericInput("N", )
+               fileInput("inputfile", NULL, buttonLabel = "Upload", multiple = FALSE),
+               tags$h6("*File upload format can be found in the 'About' tab")
+             ),
+             mainPanel(
+               tableOutput("data_view")
+             )
+    ),
+    tabPanel("About")
   )
 )
+
 ##########################################
 ### Server
 ##########################################
+
 server <- function(input, output, session) {
-  
+  output$data_view <- renderTable({
+    read <- input$inputfile
+    if (is.null(read)){
+      return()
+    }
+    read_csv(read$datapath)
+  })
 }
+
 ##########################################
 ### Knit app together 
 ##########################################
