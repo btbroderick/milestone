cf1f40e24a4274b93d435aadd43a793f57236ebd
Brendan Broderick <brendan.broderick@mayo.edu>
2018-03-15 15:01:18 -0500

    Added progress bar, and adding in the basic plot

--- app.r
+++ app.r
@@ -10,6 +10,7 @@ library(quadprog)
 library(Hmisc)
 library(msm) 
 library(VGAM)
+library(rmeta)
 source(here::here("pgm","utilsBayes1.r"))
 source(here::here("pgm","utilsFreq.r"))
 source(here::here("pgm","utilsWts.r"))
@@ -24,7 +25,7 @@ ui <- fluidPage(
     title = "Milestone",
     tabPanel("Main",
              sidebarPanel(
-               numericInput("nE", label = "Landmark Event Number", value = NA, width = 300),
+               numericInput("nE", label = "Landmark Event Number", value = 1000, width = 300),
                numericInput("N", label = "Number of simultions", value = NA, width = 300),
                dateInput("study_date",label = "Study start date", value = NA, width = 300,format = "mm-dd-yyyy"),
                tags$h6("Date format: mm-dd-yyyy"),
@@ -35,11 +36,14 @@ ui <- fluidPage(
              mainPanel(
                tabsetPanel(
                  tabPanel("Data View",
-                  dataTableOutput("data_view"),
-                  tags$h3(textOutput("data_checks"))
+                          dataTableOutput("data_view"),
+                          tags$h3(textOutput("data_checks"))
+                 ),
+                 tabPanel("Calculate Milestone",
+                          actionButton("calculate", label = "Run Milestone Prediction"),
+                          plotOutput("forestPlot",width = "200%")
                  )
-               ),
-               tabPanel("Calculate Milestone")
+               )
              )
     ),
     tabPanel("About")
@@ -69,6 +73,61 @@ server <- function(input, output, session) {
     }
   })
   
+  forestPlot <- eventReactive(input$calculate,{
+    withProgress(value = 0, message =  "Calculating", {
+      nE <- input$nE # landmark event number
+      tempdat <- inputData()
+      dat <- cbind(tempdat[[1]], tempdat[[2]]*tempdat[[3]])
+      lambda <- 0.0003255076
+      
+      #Priors
+      # Weibull prior, mean and varaince for lambda and k
+      wP< - c(lambda, 50, 1, 50)
+      
+      # Gompertz prior, mean and variance for eta and b
+      b <- lambda*log(log(2)+1)/log(2)
+      gP <- c(1, 50, b, 50)
+      
+      # Lon-logistic prior, mean and variance for alpha and beta
+      llP <- c(1/lambda, 50, 1, 50)
+      
+      # Log-normal prior, mean and varaince for mu and sigma
+      mu <- -1*log(lambda)-log(2)/2
+      lnP <- c(mu, 50, sqrt(log(2)), 50)
+      
+      cTime <- max(dat)
+      
+      incProgress(amount= .1, message = "Initialized values")
+      
+      #Frequentist Predictions
+      freqRes <- getFreqInts(dat,nE,MM=200)
+      
+      incProgress(amount= .65, message = "Frequentist Predictions")
+      
+      #Bayes predictions
+      BayesRes <- getBayesInt(dat,nE,wP,lnP,gP,llP,MM=800)
+      mean <- c(freqRes[[1]], BayesRes[[1]])
+      lower <- c(freqRes[[2]][,1], BayesRes[[2]][,1])
+      upper <- c(freqRes[[2]][,2], BayesRes[[2]][,2])
+      methodText <- cbind(c("Freq-Weibull", "Freq-LogNormal", "Freq-Gompertz", "Freq-LogLogistic",
+                            "Freq-PredSyn(Avg)", "Freq-PredSyn(MSPE)", "Freq-PredSyn(Vote)",
+                            "Bayes-Weibull", "Bayes-LogNormal", "Bayes-Gompertz", "Bayes-LogLogistic",
+                            "Bayes-PredSyn(Avg)", "Bayes-PredSyn(MSPE)", "Bayes-PredSyn(Vote)"), rep(" ", 14))
+      
+      xmin <- floor(min(lower)/50)*50
+      xmax <- ceil(max(upper)/50)*50
+      
+      incProgress(amount= .95, message = "Bayes Predictions")
+      
+      
+      forestplot(methodText, mean, lower, upper, clip = c(xmin, xmax), zero=xmin,
+                 xlab=c("Days since first pt on-study"), xticks=seq(xmin, xmax, by=100), boxsize=0.3)
+      
+      
+    })
+    
+  })
+  
   output$data_checks <- renderText({
     data_check_text()
   })
@@ -76,6 +135,10 @@ server <- function(input, output, session) {
   output$data_view <- renderDataTable({
     inputData()
   }, rownames= FALSE)
+  
+  output$forestPlot <- renderPlot({
+    forestPlot()
+  })
 }
 
 ##########################################
